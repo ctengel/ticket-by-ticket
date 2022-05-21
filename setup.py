8c6fec2a6fbe1fa95dd4b03bf874fec3fb1f3088
Chris Engelhart <engleman@gmail.com>
2022-05-21 18:25:21 -0400

    Install via pyproject.toml
    Redid basic csv2json.py as tbt-edit import
    Sort of a better version of #10
    Prep for #11

---
+++ setup.py
@@ -0,0 +1,8 @@
+"""Basic setup.py file
+
+The bulk of all info is in pyproject.toml
+This is needed to be able to do pip install -e
+"""
+from setuptools import setup
+
+setup()
--- tkt_by_tkt/editor.py
+++ tkt_by_tkt/editor.py
@@ -1,5 +1,6 @@
 """Editing TBT maps"""
 
+import argparse
 from . import models, tbtjson, gmcsv
 
 def gmcsv2tbtjson(gmcsv_file, tbtjson_file):
@@ -12,3 +13,19 @@ def gmcsv2tbtjson(gmcsv_file, tbtjson_file):
         city_obj.place(latlng)
     with open(tbtjson_file, 'w') as json_fh:
         tbtjson.write_map(my_map, json_fh)
+
+def _import(args):
+    gmcsv2tbtjson(args.gmcsv, args.tbtjson)
+
+def cli():
+    """Command Line editor interface"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tbtjson')
+    subparsers = parser.add_subparsers(help='action')
+
+    parser_import = subparsers.add_parser('import')
+    parser_import.add_argument('gmcsv')
+    parser_import.set_defaults(func=_import)
+
+    args = parser.parse_args()
+    args.func(args)
