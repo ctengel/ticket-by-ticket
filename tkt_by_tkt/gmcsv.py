e16499caf80da00f1dc194940f9208d27b39fcc6
Chris Engelhart <engleman@gmail.com>
2022-05-28 17:45:41 -0400

    Workaround for Google map CSV
    Closes #25

--- tkt_by_tkt/gmcsv.py
+++ tkt_by_tkt/gmcsv.py
@@ -6,6 +6,7 @@ def read_map(file_handle):
     """Read a Google maps CSV map and return dictionary"""
     reader = csv.DictReader(file_handle)
     # NOTE we need to reverse since Google puts long, lat vs lat, long
-    return {x['name']: tuple(reversed([float(y)
-                                       for y in x['WKT'].partition('(')[2].partition(')')[0].split()]))
-            for x in reader}
+    # NOTE sometimes Google sends an incorrect newline which confuses us hence the "if WKT"
+    return {x['name']: tuple(reversed([float(y) for y
+                                       in x['WKT'].partition('(')[2].partition(')')[0].split()]))
+            for x in reader if x['WKT']}
